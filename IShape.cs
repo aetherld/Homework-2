39e25287967ffb695e03fcbb1123913c5622f256
aetherld <124870915+aetherld@users.noreply.github.com>
2023-07-09 14:45:11 +0300

    Add files via upload

---
+++ IShape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Shape
+{
+    internal interface IShape
+    {
+        double CalculateArea();
+        double CalculatePerimeter();
+
+    }
+}
---
+++ Square.cs
@@ -0,0 +1,35 @@
+﻿namespace Shape
+{
+    public class Square : IShape
+    {
+        private double _a;
+        public double a
+        {
+            get
+            {
+                return _a;
+            }
+            set
+            {
+                if (_a <= 0)
+                {
+                    throw new Exception("Сторона не может быть равна 0");
+                }
+                _a = value;
+            }
+        }
+
+        public Square(int a)
+        {
+            _a = a;
+        }
+        public double CalculateArea()
+        {
+            return a * a;
+        }
+        public double CalculatePerimeter()
+        {
+            return a * 4;
+        }
+    }
+}
